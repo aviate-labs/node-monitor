544462453584d3b1dac8ea19a31a0ce89ad5e86e
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-07-31 14:11:51 -0700

    share state between node_monitor and flask

--- minified/node_monitor/__main__.py
+++ minified/node_monitor/__main__.py
@@ -4,22 +4,24 @@ import time
 
 app = Flask(__name__)
 
+emails = 0
+
 @app.route('/')
 def root():
     d = {
         "status": "ok",
-        "message": "Hello World!"
+        "message": "Hello World!",
+        "emails": emails
     }
     return d
 
-emails_are_being_sent = False
 
 def send_emails():
-    global emails_are_being_sent
-    emails_are_being_sent = True
+    global emails
     while True:
-        print("Sending emails")
-        time.sleep(8)
+        print("Sending email")
+        time.sleep(1)
+        emails += 1
 
 
 # Must be ran as a daemon thread so that it stops when the main thread stops
