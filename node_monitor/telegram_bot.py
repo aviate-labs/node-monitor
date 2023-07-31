3b725cb7afe90d42813cf7f2a1f2920ae594f0fb
louisevelayo <louisematiasivsegismundo.velayo@student.kuleuven.be>
2023-07-31 10:44:12 +0200

    refactor: adjust file to meet PEP8 spec

--- node_monitor/telegram_bot.py
+++ node_monitor/telegram_bot.py
@@ -7,19 +7,27 @@ class TelegramBot():
     def send_message_to_channel(self, message_content):
         try:
             request = requests.get(
-                f"https://api.telegram.org/bot{telegramBotToken}/sendMessage?chat_id={telegramChannelId}&text={message_content}")
+                f"https://api.telegram.org/bot{telegramBotToken}"
+                f"/sendMessage?chat_id={telegramChannelId}&text={message_content}"
+            )
             request.raise_for_status()
         except requests.exceptions.HTTPError as e:
             # logging.error(e)
             logging.error(
-                f"Error occurred while sending Telegram message. Check that the name of the Telegram channel id and/or Telegram Bot API token is correct")
+                f"Error occurred while sending Telegram message. "
+                f"Check that the Telegram channel id and/or Telegram bot API token is correct"
+            )
 
     def send_message_to_chat(self, message_content):
         try:
             request = requests.get(
-                f"https://api.telegram.org/bot{telegramBotToken}/sendMessage?chat_id={telegramChatId}&text={message_content}")
+                f"https://api.telegram.org/bot{telegramBotToken}"
+                f"/sendMessage?chat_id={telegramChatId}&text={message_content}"
+            )
             request.raise_for_status()
         except Exception as e:
             # logging.error(e)
             logging.error(
-                f"Error occurred while sending Telegram message. Check that the name of the Telegram chat id and/or Telegram Bot API token is correct")
+                f"Error occurred while sending Telegram message. "
+                f"Check that the Telegram chat id and/or Telegram bot API token is correct"
+            )
