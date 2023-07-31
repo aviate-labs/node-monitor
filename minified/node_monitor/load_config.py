06f2d3ef31c6812d3e58129d8abe877c2a068e19
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-07-31 15:28:39 -0700

    create partial load_config.py

---
+++ minified/node_monitor/load_config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+##############################################
+## Secrets
+
+gmailUsername    = os.environ.get('gmailUsername'  )
+gmailPassword    = os.environ.get('gmailPassword'  )
+discordBotToken  = os.environ.get('discordBotToken')  # Not implemented
+slackBotToken    = os.environ.get('slackBotToken'  )
+
+
+
+##############################################
+## Default Config
+
+## NYI
+
