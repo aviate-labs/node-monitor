acd26a94314f5318fcd9ec064ea074013d7a1d95
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-07-31 14:01:56 -0700

    get node monitor running with flask

--- minified/node_monitor/__main__.py
+++ minified/node_monitor/__main__.py
@@ -1,4 +1,35 @@
+from flask import Flask
+import threading
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    d = {
+        "status": "ok",
+        "message": "Hello World!"
+    }
+    return d
+
+emails_are_being_sent = False
+
+def send_emails():
+    global emails_are_being_sent
+    emails_are_being_sent = True
+    while True:
+        print("Sending emails")
+        time.sleep(8)
 
 
-print("NYI")
+# Must be ran as a daemon thread so that it stops when the main thread stops
+thread = threading.Thread(target=send_emails, daemon=True)
+thread.start()
+
+
+## Runs only during development
+
+if __name__ == "__main__":
+    # debug=True will run two instances of the thread
+    app.run(debug=False)
 
