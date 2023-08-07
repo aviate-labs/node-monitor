6b4fead34290114771ec5cfe6ae7737f099df8a3
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-08-07 15:48:57 -0700

    add defaults for environment variables to satisfy type checker

--- minified/node_monitor/load_config.py
+++ minified/node_monitor/load_config.py
@@ -7,10 +7,10 @@ load_dotenv()
 ##############################################
 ## Secrets
 
-gmailUsername    = os.environ.get('gmailUsername'  )
-gmailPassword    = os.environ.get('gmailPassword'  )
-discordBotToken  = os.environ.get('discordBotToken')  # Not implemented
-slackBotToken    = os.environ.get('slackBotToken'  )
+gmailUsername    = os.environ.get('gmailUsername',   '')
+gmailPassword    = os.environ.get('gmailPassword',   '')
+discordBotToken  = os.environ.get('discordBotToken', '')  # Not implemented
+slackBotToken    = os.environ.get('slackBotToken',   '')
 
 
 
