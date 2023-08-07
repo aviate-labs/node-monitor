a97667426dd6faa249821b826e96b71224a1fc31
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-08-07 10:34:35 -0700

    create telegram bot stub

---
+++ minified/node_monitor/bot_telegram.py
@@ -0,0 +1,11 @@
+import requests
+
+class TelegramBot:
+    def __init__(self, telegram_token: str):
+        self.telegram_token = telegram_token
+
+    def send_message_to_channel(self):
+        pass
+
+    def send_message_to_chat(self):
+        pass
