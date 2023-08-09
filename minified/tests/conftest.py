79990c6da066bb7bb2d8f20b89d5a505948d8ded
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-08-09 14:48:43 -0700

    add custom --send_emails command line arg to pytest

--- minified/tests/conftest.py
+++ minified/tests/conftest.py
@@ -1,9 +1,36 @@
 import pytest
 import node_monitor.ic_api as ic_api
 
+
+## Usage to actually send emails:
+##   -s is to print to stdout
+##   --send_emails is a custom flag to send live emails
+## example: pytest -s --send_emails tests/test_bot_email.py
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--send_emails",
+        action="store_true",
+        default=False,
+        help="send actual emails over the network to a test inbox"
+    )
+
 def pytest_configure(config):
     config.addinivalue_line(
         "markers", "live_email: test sends a live email over the network")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--send_emails"):
+        # --send_emails given in cli: do not skip live_email tests
+        return
+    skip_live_email = pytest.mark.skip(reason="need --send_emails option to run")
+    for item in items:
+        if "live_email" in item.keywords:
+            item.add_marker(skip_live_email)
+
+
 
 ## Create mock data for testing
 ## Data was pulled from the ic-api using cURL and stored in json files.
