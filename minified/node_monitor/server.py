9411e888882a49af7cb3e68ff6f6f05625e11555
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-08-10 14:24:26 -0700

    add nm.last_update time to the root get request

--- minified/node_monitor/server.py
+++ minified/node_monitor/server.py
@@ -11,9 +11,11 @@ def create_server(nm: NodeMonitor) -> Flask:
     @app.route('/')
     def root() -> Dict[str, str]:
         deque_length = str(len(nm.snapshots))
+        last_update = str(nm.last_update)
         d = {
             "status": "online",
             "deque_length": deque_length,
+            "last_update": last_update
         }
         return d
     # - - - - - -
