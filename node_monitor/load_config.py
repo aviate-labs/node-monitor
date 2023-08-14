07be9ddd697db63611f0644e688a579f981a20c7
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-08-14 11:14:10 -0700

    add note

--- node_monitor/load_config.py
+++ node_monitor/load_config.py
@@ -12,7 +12,9 @@ gmailPassword    = os.environ.get('gmailPassword',   '')
 discordBotToken  = os.environ.get('discordBotToken', '')  # Not implemented
 slackBotToken    = os.environ.get('slackBotToken',   '')
 
+## Pre-flight check
 # We assert that the secrets are not empty so that
 # Node Monitor will fail when run incorrectly
+# Note: it may be wise to move this into `__main__.py` instead
 assert gmailUsername != '', "Please set credentials in .env"
 assert gmailPassword != '', "Please set credentials in .env"
