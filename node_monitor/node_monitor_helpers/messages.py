71f3b1ebc80e378bd639c5e0f0b4d5a010de9374
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-08-15 13:29:00 -0700

    create function multiple_node_details

--- node_monitor/node_monitor_helpers/messages.py
+++ node_monitor/node_monitor_helpers/messages.py
@@ -1,18 +1,29 @@
-from typing import List
+from typing import List, Dict
 
 import node_monitor.ic_api as ic_api
 
+Principal = str
+
 def node_details(node: ic_api.Node, label: str) -> str:
     return (
-        f"Node ID:    {node.node_id}\n"
-        f"Node Label: {label}\n"
+        f"Node ID:     {node.node_id}\n"
+        f"Node Label:  {label}\n"
+        f"Node Status: {node.status}\n"
+        f"Live Node Status: https://dashboard.internetcomputer.org/node/{node.node_id}\n"
     )
+
+def multiple_node_details(nodes: List[ic_api.Node], 
+                          labels: Dict[Principal, str]) -> str:
+    s = ""
+    for node in nodes:
+        s += node_details(node, labels[node.node_provider_id]) + '\n'
+    return s
 
 
 def node_down_message(nodes: List[ic_api.Node]) -> str:
     s = ', '.join([node.node_id for node in nodes])
     return (
-        f"The following Nodes are down:\n"
+        f"The following Nodes are compromised:\n"
         f"{s}"
     )
 
