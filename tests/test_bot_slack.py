5e992044366ea2d193e90febd0474025bf464fbf
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-09-22 13:26:51 -0700

    update live_slack test to check for errors properly

--- tests/test_bot_slack.py
+++ tests/test_bot_slack.py
@@ -1,7 +1,9 @@
 import pytest
-import node_monitor.load_config as c
-from node_monitor.bot_slack import SlackBot  
 from unittest.mock import patch
+
+import node_monitor.load_config as c
+from node_monitor.bot_slack import SlackBot
+
 
 @patch("slack_sdk.WebClient")
 def test_send_message(mock_web_client):
@@ -20,18 +22,12 @@ def test_send_message(mock_web_client):
 
 @pytest.mark.live_slack
 def test_send_message_slack():
-    """Send real messages to a test Slack workspace"""
+    """Send a real test message to a Slack workspace"""
     slack_bot = SlackBot(c.TOKEN_SLACK)
     
-    slack_channel_name = "#node-monitor"
-    message = "Hello from test_send_message_slack()"
+    slack_channel_name = "node-monitor-allusion"
+    message = "🔬 Hello from test_send_message_slack()"
 
-    # Checks if a print statement is called within send_message().
-    # If it is called the test fails.
-    with patch('builtins.print') as mock_print:
-        try:
-            slack_bot.send_message(slack_channel_name, message)
-        except Exception as e:
-            raise AssertionError(f"An exception occurred: {e}")
-
-        mock_print.assert_not_called()
+    err = slack_bot.send_message(slack_channel_name, message)
+    if err is not None:
+        raise err
