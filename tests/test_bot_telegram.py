16630b2a0a50c5a9aa12cd28f8b2d45ea5fed80a
louisevelayo <louisevelayo@gmail.com>
2023-10-23 13:31:50 +0200

    use message templates in live telegram message testing

--- tests/test_bot_telegram.py
+++ tests/test_bot_telegram.py
@@ -2,6 +2,8 @@ import pytest
 from unittest.mock import patch
 
 import node_monitor.load_config as c
+import node_monitor.node_monitor_helpers.messages as messages
+import node_monitor.ic_api as ic_api
 from node_monitor.bot_telegram import TelegramBot
 
 @patch("requests.get")
@@ -25,8 +27,32 @@ def test_send_message(mock_get):
 def test_send_live_message():
     telegram_bot = TelegramBot(c.TOKEN_TELEGRAM)
     chat_id = "-1001925583150"  
-    message = "Test message"
 
-    err = telegram_bot.send_message(chat_id, message)
-    if err is not None:
-        raise err
+    ## Create a fake node model
+    fakenode = ic_api.Node(
+        dc_id = 'fake_dc_id',
+        dc_name = 'fake_dc_name',
+        node_id = 'fake_node_id',
+        node_operator_id = 'fake_node_operator_id',
+        node_provider_id = 'fake_node_provider_id',
+        node_provider_name = 'fake_node_provider_name',
+        owner = 'fake_owner',
+        region = 'fake_region',
+        status = 'DOWN',
+        subnet_id = 'fake_subnet_id',
+    )
+    fakelabel = {'fake_node_id': 'fake_label'}
+    fakeurl = 'https://forms.gle/thisisfake'
+    
+    subject1, message1 = messages.nodes_down_message(
+        [fakenode], fakelabel, fakeurl)
+    subject2, message2 = messages.nodes_status_message(
+        [fakenode], fakelabel, fakeurl)
+
+    err1 = telegram_bot.send_message(chat_id, message1)
+    err2 = telegram_bot.send_message(chat_id, message2)
+    if err1 is not None:
+        raise err1
+    if err2 is not None:
+        raise err2
+
