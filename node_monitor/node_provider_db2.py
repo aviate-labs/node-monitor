38041fb0c9bf0c4d3b4a496f97ada0163c585dce
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-10-27 15:50:39 -0700

    add fn get_table_as_dict and revise schema

--- node_monitor/node_provider_db2.py
+++ node_monitor/node_provider_db2.py
@@ -4,7 +4,7 @@ import psycopg2, psycopg2.extensions
 Principal = str
 
 class DictDB:
-    """Class used to operate with a postgres database using dictionaries"""
+    """Class used to operate with a postgres database using dictionaries."""
 
     def __init__(self, host: str, db: str, port: str,
                  username: str,password: str) -> None:
@@ -16,6 +16,8 @@ class DictDB:
         self.password = password
         # TODO: perhaps this isnt the right way to do this?
         self.conn: Optional[psycopg2.extensions.connection] = None
+        # TODO: see connections pooling
+        # https://www.psycopg.org/docs/pool.html
 
 
     def connect(self) -> None:
@@ -32,7 +34,7 @@ class DictDB:
         self.conn = None
 
 
-    def get_tables(self) -> List[str]:
+    def get_tables(self) -> List[Tuple[str]]:
         """List all the table names in the database"""
         assert self.conn is not None
         q = """
@@ -43,7 +45,7 @@ class DictDB:
         with self.conn.cursor() as cur:
             cur.execute(q)
             rows = cur.fetchall()
-        return [row[0] for row in rows]
+        return rows
     
 
     def get_table_schema(self, table_name: str) -> List[Tuple[str, str]]:
@@ -58,52 +60,79 @@ class DictDB:
             cur.execute(q)
             rows = cur.fetchall()
         return rows
+    
 
+    def get_table_as_dict(self, table_name: str) -> List[dict]:
+        """Get a table as a list of flat (unnested) dictionaries.
+        Example:
+        |  id  | col1 | col2 |
+        |------|------|------|
+        | val0 | val1 | val2 |
+        | val3 | val4 | val5 |
+        | val6 | val7 | val8 |
 
+        Will be returned as:
+        [
+            {"id": "val0", "col1": "val1", "col2": "val2"},
+            {"id": "val3", "col1": "val4", "col2": "val5"},
+            {"id": "val6", "col1": "val7", "col2": "val8"}
+        ]
+        """
+        assert self.conn is not None
+        q = f"""
+            SELECT *
+            FROM {table_name}
+        """
+        with self.conn.cursor() as cur:
+            cur.execute(q)
+            column_names = [desc[0] for desc in cur.description]
+            rows = cur.fetchall()
+        return [dict(zip(column_names, row)) for row in rows]
 
 
 ## Our schema
-## This is a minimum schema. 
-## Any schema that is a superset of this is valid.
+## This is a minimum schema.
+## Any schema that is a superset of this should
+## be considered valid and interoperable, and should not throw errors
+## 
+## schema = {
+##     "table_name": {
+##         "column_name": "data_type",
+##         "column_name": "data_type"
+##     }
+## }
+##
+##
 
-tables = [
-    'subscribers',
-    'email_lookup',
-    'slack_channel_lookup',
-    'telegram_chat_lookup',
-    'node_label_lookup'
-]
 
-table_subscribers = {
-    'node_provider_id': 'TEXT PRIMARY KEY',
-    'node_provider_name': 'TEXT',
-    'notify_on_status_change': 'BOOLEAN', # do we want this?
-    'notify_email': 'BOOLEAN',
-    'notify_slack': 'BOOLEAN',
-    'notify_telegram': 'BOOLEAN',
-}
-
-table_email_lookup = {
-    'id': 'SERIAL PRIMARY KEY',
-    'node_provider_id': 'TEXT',
-    'email_address': 'TEXT'
-}
-
-table_slack_channel_lookup = {
-    'id': 'SERIAL PRIMARY KEY',
-    'node_provider_id': 'TEXT',
-    'slack_channel_id': 'TEXT'
-}
-
-table_telegram_chat_lookup = {
-    'id': 'SERIAL PRIMARY KEY',
-    'node_provider_id': 'TEXT',
-    'telegram_chat_id': 'TEXT'
-}
-
-table_node_label_lookup = {
-    'node_id': 'TEXT PRIMARY KEY',
-    'node_label': 'TEXT'
+schema = {
+    "subscribers": {
+        "node_provider_id": "TEXT PRIMARY KEY",
+        "node_provider_name": "TEXT",
+        "notify_on_status_change": "BOOLEAN", # do we want this?
+        "notify_email": "BOOLEAN",
+        "notify_slack": "BOOLEAN",
+        "notify_telegram": "BOOLEAN"
+    },
+    "email_lookup": {
+        "id": "SERIAL PRIMARY KEY",
+        "node_provider_id": "TEXT",
+        "email_address": "TEXT"
+    },
+    "slack_channel_lookup": {
+        "id": "SERIAL PRIMARY KEY",
+        "node_provider_id": "TEXT",
+        "slack_channel_id": "TEXT"
+    },
+    "telegram_chat_lookup": {
+        "id": "SERIAL PRIMARY KEY",
+        "node_provider_id": "TEXT",
+        "telegram_chat_id": "TEXT"
+    },
+    "node_label_lookup": {
+        "node_id": "TEXT PRIMARY KEY",
+        "node_label": "TEXT"
+    }
 }
 
 
@@ -119,5 +148,8 @@ if __name__ == "__main__":
     print("")
     print("Schema:")
     pprint(db.get_table_schema('email_lookup'))
+    print("Subscribers as dict")
+    pprint(db.get_table_as_dict('subscribers'))
+    pprint(db.get_table_as_dict('email_lookup'))
     pprint("---------------------------------")
     db.disconnect()
