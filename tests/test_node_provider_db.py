c17d8103910408d03b20fa636e9d7c4c0344a321
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-11-09 10:08:32 -0800

    implement all db schema tests

--- tests/test_node_provider_db.py
+++ tests/test_node_provider_db.py
@@ -19,12 +19,36 @@ node_provider_db = NodeProviderDB(
         c.DB_USERNAME, c.DB_PASSWORD)
 
 
-
 @pytest.mark.db 
 def test_validate_subscribers_schema():
     expected_schema = NodeProviderDB.schema_table_subscribers
     actual_schema = node_provider_db._get_schema('subscribers')
-    from devtools import debug
-    debug(expected_schema)
-    debug(actual_schema)
+    assert expected_schema == actual_schema
+
+
+@pytest.mark.db
+def test_validate_schema_email_lookup():
+    expected_schema = NodeProviderDB.schema_table_email_lookup
+    actual_schema = node_provider_db._get_schema('email_lookup')
+    assert expected_schema == actual_schema
+
+
+@pytest.mark.db
+def test_validate_schema_slack_channel_lookup():
+    expected_schema = NodeProviderDB.schema_table_slack_channel_lookup
+    actual_schema = node_provider_db._get_schema('slack_channel_lookup')
+    assert expected_schema == actual_schema
+
+
+@pytest.mark.db
+def test_validate_schema_telegram_chat_lookup():
+    expected_schema = NodeProviderDB.schema_table_telegram_chat_lookup
+    actual_schema = node_provider_db._get_schema('telegram_chat_lookup')
+    assert expected_schema == actual_schema
+
+
+@pytest.mark.db
+def test_validate_schema_node_label_lookup():
+    expected_schema = NodeProviderDB.schema_table_node_label_lookup
+    actual_schema = node_provider_db._get_schema('node_label_lookup')
     assert expected_schema == actual_schema
