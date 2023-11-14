f848cda484a60b38434a19249af14f1f29fe5372
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-11-14 12:59:33 -0800

    import and initialize nodemonitor

--- message.py
+++ message.py
@@ -11,6 +11,19 @@ with open('email_to_send.txt', 'r') as f:
 
 ## Import nodemonitor
 from node_monitor.node_monitor import NodeMonitor
+from node_monitor.bot_email import EmailBot
+from node_monitor.node_provider_db import NodeProviderDB
+import node_monitor.load_config as c
+
+# Initialize nodemonitor
+email_bot = EmailBot(c.EMAIL_USERNAME, c.EMAIL_PASSWORD)
+node_provider_db = NodeProviderDB(
+    c.DB_HOST, c.DB_NAME, c.DB_PORT,
+    c.DB_USERNAME, c.DB_PASSWORD)
+nm = NodeMonitor(node_provider_db, email_bot, None, None)
+
+
+# Initialize broadcaster
 
 
 
