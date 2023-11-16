7fca29a2f1da2b1fb9e6c0d06de9783dda325472
louisevelayo <louisevelayo@gmail.com>
2023-11-16 13:27:57 +0100

    implement send_notifcation() for node monitor tracker

--- node_monitor_tracker/node_monitor_tracker.py
+++ node_monitor_tracker/node_monitor_tracker.py
@@ -1,12 +1,17 @@
+from typing import List
+from node_monitor.bot_email import EmailBot
 
 class NodeMonitorTracker:
-    def __init__(self, slack_token: str) -> None:
-        raise NotImplementedError
+    def __init__(self, email_bot: EmailBot, recipients: List[str]) -> None:
+        self.email_bot = email_bot
+        self.recipients = recipients
         
-    def send_notification() -> None:
-        raise NotImplementedError
+    def send_notification(self) -> None:
+        subject = "🚨 Node Monitor is Down 🚨"
+        body = "Node Monitor is down. Restart required immediately!"
+        self.email_bot.send_emails(self.recipients, subject, body)
 
-    def check_server_status() -> bool:
+    def check_server_status(self) -> bool:
         raise NotImplementedError
 
 
