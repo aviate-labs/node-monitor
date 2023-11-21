ace08f143976528e37e2b136e90e51bf60fcb06a
louisevelayo <louisevelayo@gmail.com>
2023-11-21 13:58:28 +0100

    refactor: abstract logic that checks from logic that sends notifications

--- watchdog.py
+++ watchdog.py
@@ -1,11 +1,11 @@
 import requests
-import schedule
 import time
-import node_monitor.load_config as c
 from typing import List
+
+import node_monitor.load_config as c
 from node_monitor.bot_email import EmailBot
 
-CHECK_INTERVAL = 15*60
+CHECK_INTERVAL_SECONDS = 900
 
 class Watchdog:
     def __init__(
@@ -16,30 +16,28 @@ class Watchdog:
         self.email_bot = email_bot
         self.recipients = recipients
         self.node_monitor_url = node_monitor_url
-        self.jobs = [
-            schedule.every(15).minutes.do(self.check_node_monitor_status)
-        ]
         
     def send_notification(self) -> None:
         subject = "🚨 Node Monitor is Down 🚨"
         body = "Node Monitor is down. Restart required immediately!"
         self.email_bot.send_emails(self.recipients, subject, body)
 
-    def check_node_monitor_status(self) -> bool:
+    def is_node_monitor_online(self) -> bool:
         try:
             response = requests.get(self.node_monitor_url)
+            response.raise_for_status()
             data = response.json()
-
-            if data['status'] == 'offline':
-                self.send_notification()
-
+            if data['status'] == 'online':
+                return True
+            return False
         except Exception as e:
-            self.send_notification()
+            return False
             
     def start(self) -> None:
         while True:
-            schedule.run_pending()
-            time.sleep(CHECK_INTERVAL)
+            if not self.is_node_monitor_online():
+                self.send_notification()
+            time.sleep(CHECK_INTERVAL_SECONDS)
 
 email_bot = EmailBot(c.EMAIL_USERNAME, c.EMAIL_PASSWORD)
 watchdog = Watchdog(email_bot, c.EMAIL_ADMINS_LIST, c.NODE_MONITOR_URL)
