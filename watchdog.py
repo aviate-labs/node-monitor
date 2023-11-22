76ec67ff3a7c6374ba804e8e863471ff421d1982
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-11-22 11:00:25 -0800

    add asserts

--- watchdog.py
+++ watchdog.py
@@ -1,27 +1,16 @@
-"""Watchdog Script
+#!/usr/bin/env python3
 
-This script monitors the status of a Node Monitor service and 
-sends email notifications if the service goes offline or is unreachable.
-
-Usage:
-
-1. Set the check interval in seconds (CHECK_INTERVAL_SECONDS) 
-   based on the desired frequency of monitoring. 
-   The default is 900 seconds (15 minutes).
-
-2. Define an instance of the EmailBot class with the required email credentials,
-
-
-Note:
-- The `start()` method runs indefinitely, so make sure to interrupt 
-  the script manually (^C) when needed.
-
-- It is recommended to customize the email notification content 
-  in the `send_notification()` method based on your specific requirements.
-
-- This script assumes the existence of the 'EmailBot' and 'load_config' modules, 
-  which should be available in the specified import paths.
-"""
+## Watchdog Script
+## This script monitors the status of a Node Monitor service and
+## sends email notifications if the service goes offline or is unreachable.
+##
+##
+## Usage:
+##   1. Set the check interval in seconds (CHECK_INTERVAL_SECONDS)
+##   2. Instantiate an EmailBot with the desired username, password, and (optional) SMTP server
+##   3. Instantiate class Watchdog
+##   4. Call the mainloop() method to begin monitoring
+##   2. Define an instance of the EmailBot class with the required email credentials
 
 import requests
 import time
@@ -32,7 +21,9 @@ from node_monitor.bot_email import EmailBot
 
 CHECK_INTERVAL_SECONDS = 15 * 60 # 15 minutes
 
+
 class Watchdog:
+
     def __init__(
             self, 
             email_bot: EmailBot, 
@@ -41,7 +32,7 @@ class Watchdog:
         self.email_bot = email_bot
         self.recipients = recipients
         self.node_monitor_url = node_monitor_url
-        
+
     def send_notification(self) -> None:
         subject = "🚨 Node Monitor is Down 🚨"
         body = "Node Monitor is down. Restart required immediately!"
@@ -57,17 +48,26 @@ class Watchdog:
             return False
         except Exception as e:
             return False
-            
-    def start(self) -> None:
+
+    def mainloop(self) -> None:
         while True:
             if not self.is_node_monitor_online():
                 self.send_notification()
             time.sleep(CHECK_INTERVAL_SECONDS)
 
+
+# init
 email_bot = EmailBot(c.EMAIL_USERNAME, c.EMAIL_PASSWORD)
 watchdog = Watchdog(email_bot, c.EMAIL_ADMINS_LIST, c.NODE_MONITOR_URL)
 
-assert watchdog.is_node_monitor_online() == True
+
+# pre-flight check
+assert c.EMAIL_USERNAME != '', "Please set email credentials in .env"
+assert c.EMAIL_PASSWORD != '', "Please set email credentials in .env"
+assert c.EMAIL_ADMINS_LIST != '', "Please set email credentials in .env"
+assert c.NODE_MONITOR_URL != '', "Please set email credentials in .env"
+assert watchdog.is_node_monitor_online() == True, "Init fail: Node Monitor is offline"
+
 
 if __name__ == "__main__":
-    watchdog.start()
+    watchdog.mainloop()
