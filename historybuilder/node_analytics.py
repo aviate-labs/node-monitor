3c130c7e3cc63f12bbfe999e3dbf3d79b68c7889
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-11-29 15:03:26 -0800

    use pandas

--- historybuilder/node_analytics.py
+++ historybuilder/node_analytics.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+import pandas as pd
 from historybuilder_py.HistoryBuilderDB import HistoryBuilderDB
-from devtools import debug
 
 target = "2cov2-kcta2-rsiae-jvmot-jhp4z-kmq7n-75e35-4dvf7-tobjm-ml4lt-bqe"
 
@@ -15,13 +15,13 @@ def get_status(parsed_json: dict):
 
 
 if __name__ == "__main__":
-    from devtools import debug
     db = HistoryBuilderDB()
     rows = db.get_between(1701200280, 1701200438)
-    # rows = [(row[0], row[1], row[3]) for row in rows]
-    for row in rows:
-        time, uuid, parsed_json = row
-        status = get_status(parsed_json)
-        print(f"{time}    {uuid}    {status}")
-    # debug(rows)
+    
+    # Create a pandas DataFrame from the rows
+    df = pd.DataFrame(rows, columns=['time', 'uuid', 'parsed_json'])
 
+    # Apply the get_status function to the 'parsed_json' column
+    df['status'] = df['parsed_json'].apply(get_status)
+
+    print(df[['time', 'uuid', 'status']])
