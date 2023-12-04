91b1e7daa7cb93e7a1ab5b7e974b76496aa6bf5a
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2023-12-04 13:41:26 -0800

    use itertools.windowed to split up the function call

--- historybuilder/node_analytics.py
+++ historybuilder/node_analytics.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import plotly.express as px
 from historybuilder_py.HistoryBuilderDB import HistoryBuilderDB
+import more_itertools
 
 target = "2cov2-kcta2-rsiae-jvmot-jhp4z-kmq7n-75e35-4dvf7-tobjm-ml4lt-bqe"
 
@@ -28,16 +29,40 @@ if __name__ == "__main__":
     t2 = HistoryBuilderDB.datetime_to_epoch_seconds("2023-11-04")
     print(t1, t2)
     # rows = db.get_between(1696006820, 1696009820)
-    rows = db.get_between(t1, t2)
+    intervals = range(t1, t2, 3600) # 3600 seconds / 60 = 60 minutes
+    windows = more_itertools.windowed(intervals, 2)
+
+    def list_2_dataframe(t1, t2) -> pd.DataFrame:
+        """Returns a dataframe with the following columns:
+        time, status, datetime, status_value"""
+        l = db.get_between(t1, t2)
+        df = pd.DataFrame(l, columns=['time', 'uuid', 'parsed_json'])
+        df['status'] = df['parsed_json'].apply(get_status)
+        df['datetime'] = df['time'].apply(HistoryBuilderDB.epoch_seconds_to_datetime)
+        df = df.drop('uuid', axis=1)         # drop column uuid
+        df = df.drop('parsed_json', axis=1)
+        df['status_value'] = df['status'].apply(lambda x: 1 if x == 'UP' else 0)
+        return df
+
+    for window in windows:
+        print(window)
+        t0, t1 = window
+        df = list_2_dataframe(t0, t1)
+        print(df)
+    
+    # print(df)
+    print("Done!")
+
+    # rows = db.get_between(t1, t2)
 
     # Create dataframe
-    df = pd.DataFrame(rows, columns=['time', 'uuid', 'parsed_json'])
-    df['status'] = df['parsed_json'].apply(get_status)
-    df['datetime'] = df['time'].apply(HistoryBuilderDB.epoch_seconds_to_datetime)
+    # df = pd.DataFrame(rows, columns=['time', 'uuid', 'parsed_json'])
+    # df['status'] = df['parsed_json'].apply(get_status)
+    # df['datetime'] = df['time'].apply(HistoryBuilderDB.epoch_seconds_to_datetime)
 
-    df = df.drop('uuid', axis=1)         # drop column uuid
-    df = df.drop('parsed_json', axis=1)  # drop column parsed_json
-    df['status_value'] = df['status'].apply(lambda x: 1 if x == 'UP' else 0)
+    # df = df.drop('uuid', axis=1)         # drop column uuid
+    # df = df.drop('parsed_json', axis=1)  # drop column parsed_json
+    # df['status_value'] = df['status'].apply(lambda x: 1 if x == 'UP' else 0)
 
     # Calculate uptime percentage
     total_data_points = df.shape[0]
